c2b7a10b30d654c0fa19d097318116c4b2aa1e45
Masatoshi Higuchi <2480630+ogis-higuchi@users.noreply.github.com>
2024-01-31 01:22:10 +0000

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,32 @@
+import { awscdk, javascript } from 'projen';
+
+const project = new awscdk.AwsCdkTypeScriptApp({
+  name: '@ogis-rd/awscdk-nat-gw-switch',
+  description: 'AWS CDK app to turn NAT gateways on and off',
+
+  buildWorkflow: false,
+  depsUpgradeOptions: {
+    workflow: false,
+  },
+  githubOptions: {
+    pullRequestLint: false,
+  },
+  pullRequestTemplate: false,
+
+  cdkVersion: '2.73.0',
+  deps: [
+    '@ogis-rd/awscdk-nat-lib@^0',
+  ],
+
+  packageManager: javascript.NodePackageManager.NPM,
+  projenrcTs: true,
+
+  repository: 'https://github.com/ogis-rd/awscdk-nat-gw-switch.git',
+  defaultReleaseBranch: 'main',
+
+  authorName: 'OGIS-RI Co.,Ltd.',
+  authorUrl: 'https://www.ogis-ri.co.jp',
+  authorOrganization: true,
+});
+
+project.synth();
---
+++ README.md
@@ -0,0 +1,10 @@
+> [!WARNING]
+> This repository is under construction.
+
+# awscdk-nat-gw-switch
+
+AWS CDK app to turn NAT gateways on and off.
+
+## License
+
+This project is licensed under the [Apache-2.0](LICENSE) license.
